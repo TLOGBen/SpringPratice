689140d81fde30e55c57e917a8a8066befc4fdd2
tlogben <powdersave@gmail.com>
2021-04-13 06:57:07 +0800

    調整設定

--- src/main/java/com/example/demo/configuration/JPAConfig.java
+++ src/main/java/com/example/demo/configuration/JPAConfig.java
@@ -1,9 +1,10 @@
 package com.example.demo.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -19,13 +20,9 @@ import javax.sql.DataSource;
 public class JPAConfig {
 
     @Bean
+    @ConfigurationProperties(prefix = "primary.datasource")
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/test_1");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("oooo0000");
-        return dataSource;
+        return DataSourceBuilder.create().build();
     }
 
     @Bean
